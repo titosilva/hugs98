b0a45d3c246033ee5d5bc406585acea0daa69ea1
titosilva <jt.mat@hotmail.com>
2020-10-12 13:12:23 -0300

    fix: adding operator /\ to question 2 solution

--- exercises/Q2/q2.hs
+++ exercises/Q2/q2.hs
@@ -4,3 +4,6 @@ intersect :: [Double] -> [Double] -> [Double]
 intersect lx ly = filter (elem_rev ly) lx where{
     elem_rev l item = elem item l;
 }
+
+(/\) :: [Double] -> [Double] -> [Double]
+lx /\ ly = intersect lx ly
